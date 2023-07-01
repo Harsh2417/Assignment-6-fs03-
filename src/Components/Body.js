326b69949f283aa1e6d7bb01b6d9c2ee179f2e85
Harsh2417 <65455754+Harsh2417@users.noreply.github.com>
2023-07-01 16:57:07 +0530

    Add files via upload

---
+++ src/Components/Body.js
@@ -0,0 +1,14 @@
+function Body(){
+    return(
+        <div>
+            <div style={{backgroundColor:"#492860",height:"220px",textAlign:"center",padding:"30px 0",color:"#184F91"}}>
+                <h1 style={{color:'white'}}>See value from social sooner</h1>
+                <p style={{margin:"30px 0",color:'white'}}> Our Training and strategic services helped housands of organizations succeed on social </p>
+                <button style={{height:
+                    "40px",width:"120px",backgroundColor:"green",color:"white",fonts
+                    :"16px",border:"none",borderRadius:"8px",cursor:"pointer"}}>Request a demo</button>
+            </div>
+        </div>
+    )
+}
+export default Body;
